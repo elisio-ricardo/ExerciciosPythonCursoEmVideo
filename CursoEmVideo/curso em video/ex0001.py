7f377f6fa104e0a14723ca629fc35e8e17719038
elisio ricardo canuto da silva <elisiorcsilva@hotmail.com>
2020-11-13 14:24:51 -0300

    Update ex0001.py

--- CursoEmVideo/curso em video/ex0001.py
+++ CursoEmVideo/curso em video/ex0001.py
@@ -2,4 +2,3 @@ n1 = int(input(' digite o primeiro valor '))
 n2 = int(input(' Digite o segundo valor '))
 s = n1 + n2
 print(" A soma dos valores {} e {} sera {}" .format(n1, n2, s))
-print("Outra mudança")
